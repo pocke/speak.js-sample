0d7915b446516ac4f496e2e84da14cf849cc3200
pocke <p.ck.t22@gmail.com>
2014-09-07 09:06:11 +0900

    workaround
    gh-pages cant use symlink to git-submodule ?

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+var speakWorker;
+try {
+  speakWorker = new Worker('vendor/speak-js/speakWorker.js');
+} catch(e) {
+  console.log('speak.js warning: no worker support');
+}
+
+
 (function () {
   
   var text = function () {
--- speakGenerator.js
+++
@@ -1,1 +0,0 @@
-vendor/speak-js/speakGenerator.js
